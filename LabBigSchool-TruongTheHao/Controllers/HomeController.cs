cd6ad01c3653989797d1c2bf929069a2c397d702
flower1st <haothetruong257@gmail.com>
2022-06-06 11:06:35 +0700

    tuan 2

--- LabBigSchool-TruongTheHao/Controllers/HomeController.cs
+++ LabBigSchool-TruongTheHao/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using LabBigSchool_TruongTheHao.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace LabBigSchool_TruongTheHao.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDBContext _dbContext;
+
+        public HomeController()
+        {
+            _dbContext = new ApplicationDBContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.Datetime > DateTime.Now);
+
+            return View(upcomingCourses);
         }
 
         public ActionResult About()
